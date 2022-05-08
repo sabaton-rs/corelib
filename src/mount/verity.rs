9a8d740f4530a04de52e36654d4dfdedc4a5189d
Sojan James <sojan.james@gmail.com>
2022-05-08 14:12:48 +0530

    check return value

--- src/mount/verity.rs
+++ src/mount/verity.rs
@@ -4,12 +4,15 @@ use crate::error::CoreError;
 
 pub fn load_dm() -> Result<(), CoreError> {
     log::info!("load_dm");
-    let dm = DM::new().unwrap();
+    if let Ok(dm) = DM::new() {
     let (maj,min,patch) = dm.version()
         .map_err(|e|{
             log::error!("Unable to get DM version: {}", e);
             CoreError::DMError
         })?; 
         log::info!("DM version:{} {} {}", maj,min,patch);
+    } else {
+        log::error!("DM Init failed");
+    }
     Ok(())
 }
